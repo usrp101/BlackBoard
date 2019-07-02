1832d54d213738964840380ee6ddb6c6548be4b4
kevinleemiguel <iradukundaleekevin@gmail.com>
2019-07-02 10:42:41 +0200

    spelling error

--- blackboard2/src/main/java/com/example/demo/Domain/Announcement.java
+++ blackboard2/src/main/java/com/example/demo/Domain/Announcement.java
@@ -5,10 +5,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 @Entity
-public class Annoncement {
+public class Announcement {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int anId;
