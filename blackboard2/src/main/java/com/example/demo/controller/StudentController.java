52ae3cf2a35aed7850c8d1d3fda6c4862b19359f
Ndahigeze <yesselini19@gmail.com>
2019-08-06 19:44:50 +0200

    Made changes

--- blackboard2/src/main/java/com/example/demo/controller/StudentController.java
+++ blackboard2/src/main/java/com/example/demo/controller/StudentController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.Domain.Course;
 import com.example.demo.Domain.Student;
+import com.example.demo.Domain.Student_course;
+import com.example.demo.Service.CourseService;
 import com.example.demo.Service.StudentService;
+import com.example.demo.Service.Student_courseService;
 import com.example.demo.util.Messages;
 import com.example.demo.util.ResponseBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,11 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    @Autowired
+    private CourseService courseService;
+
+    @Autowired
+    private Student_courseService stCourse;
     @RequestMapping(value = "/save", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> save(@RequestBody Student st) {
         ResponseBean responseBean = new ResponseBean();
@@ -44,10 +53,69 @@ public class StudentController {
         return new ResponseEntity<Object>(responseBean, HttpStatus.OK);
     }
 
+
+
+//    Saving oneStudent
+    @RequestMapping(value = "/saveone", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> saveOne(@RequestBody InnerStudent st) {
+        ResponseBean responseBean = new ResponseBean();
+        //TODO: process POST request
+        try {
+            Student u=studentService.findByStudentId(st.id);
+            Course c=courseService.findByUuid(st.courseUuid);
+            if(c!=null){
+                if(u==null){
+                    Student student=new Student();
+                    student.setEmail(st.email);
+                    student.setFirstname(st.name);
+                    student.setStudentId(st.id);
+                    student.setLastname("");
+                    Student nu=studentService.create(student);
+
+                    Student ost=studentService.findByStudentId(st.id);
+                    if(ost!=null){
+                        Student_course stc=new Student_course();
+                        stc.setStudent(ost);
+                        stc.setCourse(c);
+                        stCourse.create(stc);
+                        responseBean.setCode(Messages.SUCCESS_CODE);
+                        responseBean.setDescription(Messages.save);
+                    }else{
+                        responseBean.setCode(Messages.ERROR_CODE);
+                        responseBean.setDescription(Messages.error);
+                    }
+
+                }else{
+                    Student_course stc=new Student_course();
+                    stc.setStudent(u);
+                    stc.setCourse(c);
+                    stCourse.create(stc);
+                    responseBean.setCode(Messages.ERROR_CODE);
+                    responseBean.setDescription("Student of this id is already registered");
+                    responseBean.setObject(null);
+                }
+
+            }else{
+                responseBean.setCode(Messages.ERROR_CODE);
+                responseBean.setDescription("Course Not Found");
+                responseBean.setObject(null);
+            }
+
+        } catch (Exception e) {
+            //TODO: handle exception
+            responseBean.setCode(Messages.ERROR_CODE);
+            responseBean.setDescription(Messages.error);
+            responseBean.setObject(null);
+        }
+        return new ResponseEntity<Object>(responseBean, HttpStatus.OK);
+    }
+
+
+
     /**
      * Update
      * @param uuid
-     * @param user
+
      * @return
      */
     @RequestMapping(value = "/update/{uuid}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -148,6 +216,49 @@ public class StudentController {
             responseBean.setObject(null);
         }
         return new ResponseEntity<Object>(responseBean, HttpStatus.OK);
+    }
+
+
+
+
+
+    private static class InnerStudent{
+        private String name;
+        private String id;
+        private String email;
+        private String courseUuid;
+
+        public String getCourseUuid() {
+            return courseUuid;
+        }
+
+        public void setCourseUuid(String courseUuid) {
+            this.courseUuid = courseUuid;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
     }
 
 }
